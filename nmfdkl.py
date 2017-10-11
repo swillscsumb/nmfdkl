5793b3bdac496ae4710ceed4a8ef2ec9a035c214
Shawn Wills <swills@csumb.edu>
2017-10-11 07:05:16 -0700

    Add documentation.
    Add constant for H/weights template index

--- nmfdkl.py
+++ nmfdkl.py
@@ -3,20 +3,55 @@ import warnings
 import numpy as np
 
 class NMFDKL(object):
-    """Based on https://github.com/romi1502/NMF-matlab/blob/ by Romain
-        Hennequin
+    """
+    Based on https://github.com/romi1502/NMF-matlab/blob/ by Romain
+    Hennequin
+
+    Implements Non-negative Matrix Factor Deconvolution (NMFD) as
+    proposed by Smaragdis.
+
+    Smaragdis, P. (2004). Non-negative matrix factor deconvolution;
+        extraction of multiple sound sources from monophonic inputs.
+        Lecture Notes in Computer Science (including subseries Lecture
+        Notes in Artificial Intelligence and Lecture Notes in
+        Bioinformatics), 3195, 494-499. Retrieved from
+        http://www.merl.com/reports/docs/TR2004-104.pdf
     """
     _EPSILON = np.spacing(1)
+    _H_IDX = 0
 
     def __init__(
              self, matrix, factors, bases_size, bases=None, weights_size=1,
              weights=None
              ):
         """
+        nmfdkl.NMFDKL(
+            matrix, factors, bases_size, bases=None, weights_size=1,
+            weights=None)
+        Parameters
+        ----------
+        matrix : array_like
+            V MxN matrix to factor
+        factors : int
+            R decomposition components
+        bases_size : int
+            size of bases factor templates
+        bases : array_like
+            W initial MxTxR
+        weights_size : int
+            holding place for future NMF2D implementation.
+            size of weights factor templates
+        weights : array_like
+            H initial RxTxN matrix
+        Returns
+        -------
+        NMFD()
         """
         self._factors = None
         # V
         self.matrix = matrix
+        self.matrix_max = np.max(matrix)
+        self.matrix_inv_max = 1.0 / self.matrix_max
         self.factors = factors
         # W
         if bases is not None:
@@ -27,7 +62,7 @@ class NMFDKL(object):
             self.bases = bases.copy()
         else:
             self.bases = np.random.rand(self.rows, bases_size, self.factors)
-            self.bases *= np.max(self.matrix)
+            self.bases *= self.matrix_max
         # H
         if weights is not None:
             assert(
@@ -38,7 +73,7 @@ class NMFDKL(object):
         else:
             self.weights = np.random.rand(
                 self.factors, weights_size, self.columns)
-            self.weights *= np.max(self.matrix)
+            self.weights *= self.matrix_max
 
         self.one = np.ones((self.rows, self.columns))
 
@@ -59,7 +94,18 @@ class NMFDKL(object):
         return self.matrix.shape[1]
 
     def shift_column(self, a, shift):
-        """shift array n columns
+        """
+        shift array n columns
+        Parameters
+        ----------
+        a : array_like
+            array to shift
+        shift : int
+            number of indexes to shift
+        Returns
+        -------
+        out : array_like
+            shifted array
         """
         if shift == 0:
             return a
@@ -71,7 +117,18 @@ class NMFDKL(object):
         return a_roll
 
     def shift_row(self, a, shift):
-        """shift array n rows
+        """
+        shift array n rows
+        Parameters
+        ----------
+        a : array_like
+            array to shift
+        shift : int
+            number of indexes to shift
+        Returns
+        -------
+        out : array_like
+            shifted array
         """
         if shift == 0:
             return a
@@ -86,27 +143,37 @@ class NMFDKL(object):
         """
         Create exponential decay envelope
         y = y0 * e ** (k*t)
+        Parameters
+        ----------
+        max : float
+            peak value of envelope
+        t : int
+            length of envelope
+        Returns
+        -------
+        out : array_like
+            t length array
         """
         k = np.log(self._EPSILON/max)/t
         return np.fromfunction(lambda i: max*np.exp(k*i), (t,))
 
     def lambda_(self):
-        """L = sum((W)(H))
+        """
+        L = sum((W)(H))
         """
         lam = np.zeros((self.rows, self.columns))
-        weights_cs = None
-        for t in xrange(self.bases.shape[1]):
-            if weights_cs is None:
-                weights_cs = self.weights[:, 0, :]
-            else:
-                # right shift the prior array so we only have to zero one
-                # column
-                weights_cs = self.shift_column(weights_cs, 1)
-            lam += np.dot(self.bases[:, t, :], weights_cs)
+
+        for m in xrange(self.rows):
+            for r in xrange(self.factors):
+                lam[m, :] += np.convolve(
+                    self.bases[m, :, r],
+                    self.weights[r, _H_IDX, :])[:self.columns]
+
         return lam
 
     def v_over_lambda(self):
-        """V/L
+        """
+        V/L
         """
         lam = self.lambda_()
         vol = self.matrix/(lam + self._EPSILON)
@@ -116,9 +183,11 @@ class NMFDKL(object):
         """
         """
         vol_cs = None
-        weights_tmp = np.zeros((self.factors, self.columns))
+        weights_num = np.zeros((self.factors, self.columns))
+        weights_den = np.zeros((self.factors, self.columns))
+
         for t in xrange(self.bases.shape[1]):
-            weights_cs = self.shift_column(self.weights[:, 0, :], t).T
+            weights_cs = self.shift_column(self.weights[:, _H_IDX, :], t).T
             self.bases[:, t, :] *= (
                 np.dot(vol, weights_cs)
                 /(np.dot(self.one, weights_cs) + self._EPSILON))
@@ -129,19 +198,20 @@ class NMFDKL(object):
                 # left shift the prior array so we only have to zero one
                 # column
                 vol_cs = self.shift_column(vol_cs, -1)
+            weights_num += np.dot(bases_t, vol_cs)
+            weights_den += np.dot(bases_t, self.one)
 
-            weights_tmp += (
-                np.dot(bases_t, vol_cs)
-                /(np.dot(bases_t, self.one) + self._EPSILON))
-        self.weights[:, 0, :] *= weights_tmp
+        self.weights[:, _H_IDX, :] *= weights_num/(weights_den + self._EPSILON)
 
     def update_bases(self, vol):
-        """W = W * (((V/L)(H))/(1(H)))
+        """
+        dot product bases update
+        W = W * (((V/L)(H))/(1(H)))
         """
         weights_cs = None
         for t in xrange(self.bases.shape[1]):
             if weights_cs is None:
-                weights_cs = self.weights[:, 0, :].T
+                weights_cs = self.weights[:, _H_IDX, :].T
             else:
                 # use shift_row since we're shifting the transpose
                 weights_cs = self.shift_row(weights_cs, 1)
@@ -150,10 +220,13 @@ class NMFDKL(object):
                 /(np.dot(self.one, weights_cs) + self._EPSILON))
 
     def update_weights(self, vol):
-        """H = H * (((W.T)(V/L))/((W)1))
+        """
+        dot product weights update
+        H = H * (((W.T)(V/L))/((W)1))
         """
         vol_cs = None
-        weights_tmp = np.zeros((self.factors, self.columns))
+        weights_num = np.zeros((self.factors, self.columns))
+        weights_den = np.zeros((self.factors, self.columns))
         for t in xrange(self.bases.shape[1]):
             if vol_cs is None:
                 vol_cs = vol
@@ -162,84 +235,119 @@ class NMFDKL(object):
                 # column
                 vol_cs = self.shift_column(vol_cs, -1)
             bases_t = self.bases[:, t, :].T
-            weights_tmp += (
-                np.dot(bases_t, vol_cs)
-                /(np.dot(bases_t, self.one) + self._EPSILON))
+            weights_num += np.dot(bases_t, vol_cs)
+            weights_den += np.dot(bases_t, self.one)
 
-        self.weights[:, 0, :] *= weights_tmp
+        self.weights[:, _H_IDX, :] *= weights_num/(weights_den + self._EPSILON)
+
+    def update_weights_conv(self, vol):
+        """
+        convolution weights update
+        H = H * (((W.T)(V/L))/((W)1))
+        """
+        vol_lr = np.fliplr(vol)
+        weights_num = np.zeros((self.factors, self.columns))
+        weights_den = np.zeros((self.factors, self.columns))
+        for r in xrange(self.factors):
+            for m in xrange(self.rows):
+                weights_num[r, :] += np.convolve(
+                    vol_lr[m, :], self.bases[m, :, r])[:self.columns]
+        weights_num = np.fliplr(weights_num)
+        for t in xrange(self.bases.shape[1]):
+            bases_t = self.bases[:, t, :].T
+            weights_den += np.dot(bases_t, self.one)
+        self.weights[:, _H_IDX, :] *= weights_num/(weights_den + self._EPSILON)
 
     def update_weights_avg(self, vol):
         """
-        average along t
+        Hennequin convolution weights update with averaging
         """
         hu = np.zeros((self.factors, self.columns))
         hd = np.zeros((self.factors, self.columns))
         for r in xrange(self.factors):
             for m in xrange(self.rows):
                 bases_flip = np.flipud(self.bases[m, :, r])
-                cv = np.convolve(vol[m, :], bases_flip, 'same')
-                hu[r, :] += cv
-                cv = np.convolve(self.one[m, :], bases_flip, 'same')
-                hd[r, :] += cv
+                hu[r, :] += np.convolve(vol[m, :], bases_flip)[:self.columns]
+                hd[r, :] += np.convolve(
+                    self.one[m, :], bases_flip)[:self.columns]
 
         # average along t
-        self.weights[:, 0, :] *= hu/hd
+        self.weights[:, _H_IDX, :] *= hu/hd
 
     def get_cost(self, vol, lam):
         """
+        Kullback-Leibler cost
         D = ||V * ln(V/L) - V + L||
                                    F
         """
         cost = np.linalg.norm(
-            self.matrix*np.log(vol + self._EPSILON) - self.matrix + lam, 'fro')
+            self.matrix*np.log(vol + self._EPSILON) - self.matrix + lam,
+            'fro') * self.matrix_inv_max
         return cost
 
     def nmfdkl_iter(self, iterations):
-        """Dittmar and Muller iteration algoritm
+        """
+        Hennequin iteration algoritm
         """
         for i in xrange(iterations):
             warnings.warn("iteration: {0:d}".format(i+1))
             vol, lam = self.v_over_lambda()
             cost = self.get_cost(vol, lam)
             warnings.warn("cost: {0}".format(str(cost)))
-            self.update_weights_avg(vol)
-            vol, lam = self.v_over_lambda()
+
             self.update_bases(vol)
+            vol, lam = self.v_over_lambda()
+            self.update_weights_avg(vol)
 
     def nmfdkl_dm_iter(self, iterations):
-        """Dittmar and Muller iteration algoritm
+        """
+        Dittmar and Muller like iteration algoritm
         """
         for i in xrange(iterations):
             warnings.warn("iteration: {0:d}".format(i+1))
             vol, lam = self.v_over_lambda()
             cost = self.get_cost(vol, lam)
             warnings.warn("cost: {0}".format(str(cost)))
-            self.update_bases_weights_t(vol, lam)
+            #self.update_bases_weights_t(vol, lam)
+            self.update_bases(vol)
+            vol, lam = self.v_over_lambda()
+            self.update_weights_conv(vol)
 
     def post_process_rh(self):
-        """Post processing from NMF-matlab Romain Hennequin
         """
-        weights_max = np.max(self.weights[:, 0, :])/10.0
-        self.weights[:, 0, :] = (
-            np.maximum(self.weights[:, 0, :], weights_max)
+        Post processing from NMF-matlab Romain Hennequin
+        """
+        weights_max = np.max(self.weights[:, _H_IDX, :])/10.0
+        self.weights[:, _H_IDX, :] = (
+            np.maximum(self.weights[:, _H_IDX, :], weights_max)
             - weights_max + self._EPSILON)
 
     def post_process(self):
-        """Insert exponential decay envelopes in weights
         """
-        d_env = self.decay(np.max(self.weights[:, 0, :]), self.bases.shape[1])
+        Insert exponential decay envelopes in weights
+        """
+        d_env = self.decay(np.max(self.weights[:, _H_IDX, :]), self.bases.shape[1])
         for r in xrange(self.factors):
-            self.weights[r, 0, :] = np.convolve(
-                self.weights[r, 0, :], d_env, 'same')
+            self.weights[r, _H_IDX, :] = np.convolve(
+                self.weights[r, _H_IDX, :], d_env)[:self.weights.shape[2]]
 
     def reconstruct(self):
         """
+        reconstruct component factor matrices
+        Parameters
+        ----------
+
+        Returns
+        -------
+        v_out : array_like
+            MxNxR
         """
         v_out = np.zeros((self.rows, self.columns, self.factors+1))
         for r in xrange(self.factors):
             for m in xrange(self.rows):
                 v_out[m, :, r] += np.convolve(
-                    self.weights[r, 0, :], self.bases[m, :, r], 'same')
+                    self.weights[r, _H_IDX, :],
+                    self.bases[m, :, r])[:self.columns]
                 v_out[m, :, -1] += v_out[m, :, r]
         return v_out
 
@@ -247,7 +355,7 @@ class NMFDKL(object):
     def nmfdkl(self, pre, post):
         """
         """
-        self.nmfdkl_iter(pre)
+        self.nmfdkl_dm_iter(pre)
         self.post_process_rh()
-        self.nmfdkl_iter(post)
+        self.nmfdkl_dm_iter(post)
         return self.reconstruct()
